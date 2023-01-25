10dbc9ff3ec4d239edc9a0eeca2a1a17da3627a7
Ivan <96646371+Rayilsh@users.noreply.github.com>
2023-01-25 22:44:42 +0300

    update server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://ai-zm79.onrender.com', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
